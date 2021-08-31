ade8e00f2aee74d52f223324645b50b1871b1a6f
tersawy <mohamedtersawy96@gmail.com>
2021-08-31 02:04:35 +0200

    Added Language Seeder file

---
+++ server/database/seeds/LanguageSeed.js
@@ -0,0 +1,16 @@
+const Language = require("../../Models/Language");
+
+module.exports = {
+	model: Language,
+	data: [
+		{ name: "arabic" },
+		{ name: "english" },
+		{ name: "chinese" },
+		{ name: "french" },
+		{ name: "german" },
+		{ name: "spanish" },
+		{ name: "italiano" },
+		{ name: "russian" },
+		{ name: "turkish" },
+	],
+};
