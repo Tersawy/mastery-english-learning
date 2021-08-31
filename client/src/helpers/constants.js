50c5dbf3f607c7e0fb3341043dc18093cc0d2ca3
tersawy <mohamedtersawy96@gmail.com>
2021-08-31 02:28:24 +0200

    Added frontend constants

---
+++ client/src/helpers/constants.js
@@ -0,0 +1,15 @@
+export const COURSE_PENDING = 0;
+export const COURSE_APPROVED = 1;
+export const COURSE_STATUS = [COURSE_PENDING, COURSE_APPROVED];
+export const COURSE_STATUS_STR = ["Pending", "Approved"];
+
+export const COURSE_LEVEL_BEGINNER = 0;
+export const COURSE_LEVEL_ADVANCED = 1;
+export const COURSE_LEVEL_INTERMEDIATE = 2;
+export const COURSE_LEVELS = [COURSE_LEVEL_BEGINNER, COURSE_LEVEL_ADVANCED, COURSE_LEVEL_INTERMEDIATE];
+export const COURSE_LEVELS_STR = ["Beginner", "Advanced", "Intermediate"];
+
+export const STUDENT = 0;
+export const INSTRUCTOR = 1;
+export const ADMIN = 2;
+export const OWNER = 3;
