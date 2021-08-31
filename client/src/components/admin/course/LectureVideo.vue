f4f38db5a6759bb2b19de1396ec2bc2f5a3ac42e
tersawy <mohamedtersawy96@gmail.com>
2021-08-31 02:46:48 +0200

    Added lecture video component

---
+++ client/src/components/admin/course/LectureVideo.vue
@@ -0,0 +1,30 @@
+<template>
+	<b-modal id="lectureVideo" @hidden="$emit('closed')" hide-footer size="xl" centered body-class="p-0">
+		<template #modal-header="{ close }">
+			<div class="d-flex align-items-center justify-content-between w-100">
+				<div class="d-flex align-items-center">
+					<h6 class="mb-0">
+						Lecture:&nbsp;
+						<span class="font-sm font-weight-700" v-b-tooltip="lecture.title">{{ lecture.title | strLength(30) }}</span>
+					</h6>
+				</div>
+				<b-button size="sm" variant="outline-danger" @click="close()"> Close </b-button>
+			</div>
+		</template>
+		<template #default>
+			<video class="video-review d-flex" controls :src="`${lecturesURL}/${lecture.video}`" ref="videoReview">Your browser does not support the video tag.</video>
+		</template>
+	</b-modal>
+</template>
+
+<script>
+	export default {
+		computed: {
+			lecture() {
+				return this.$store.state.Course.oneLecture;
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped></style>
