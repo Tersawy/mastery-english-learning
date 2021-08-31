892b53847e666801189b38da826774bca164f8c0
tersawy <mohamedtersawy96@gmail.com>
2021-08-31 02:40:22 +0200

    Added ReportCard Component

---
+++ client/src/components/DashboardLinks.vue
@@ -0,0 +1,45 @@
+<template>
+	<ul class="sidebar-list list-unstyled mb-0">
+		<li class="sidebar-item" v-for="(item, i) in items" :key="i">
+			<router-link :to="item.path" :exact="i == 0" class="p-3 d-flex align-items-center text-decoration-none">
+				<h4 class="mb-0 mr-3">
+					<b-icon :icon="item.icon"></b-icon>
+				</h4>
+				<span class="">{{ item.text }}</span>
+			</router-link>
+		</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				items: [
+					{ text: "Dashboard", icon: "grid", path: "/dashboard" },
+					{ text: "Courses", icon: "collection", path: "/dashboard/courses" },
+					{ text: "Students", icon: "people", path: "/dashboard/students" },
+					{ text: "Instructors", icon: "person-plus", path: "/dashboard/instructors" },
+					{ text: "Enrollments", icon: "diagram3", path: "/dashboard/enrollments" },
+					{ text: "Enroll a Student", icon: "bezier2", path: "/dashboard/enroll-student" }
+				]
+			};
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.sidebar-list {
+		.sidebar-item {
+			& > a {
+				color: #333;
+				&.router-link-active,
+				&:hover {
+					background: var(--primary);
+					color: white;
+					font-weight: 600 !important;
+				}
+			}
+		}
+	}
+</style>
