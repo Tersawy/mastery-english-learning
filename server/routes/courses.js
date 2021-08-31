2ab816ed4ba5c7fb57c7350e1c589ee4b6b47f28
tersawy <mohamedtersawy96@gmail.com>
2021-08-31 02:14:01 +0200

    Added courses routes

---
+++ server/routes/courses.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+const CourseController = require("../Controllers/CourseController");
+
+const { auth, instructorAndAdmin } = require("../middlewares/auth");
+
+router.get("/", auth, instructorAndAdmin, CourseController.all);
+
+router.get("/:courseId", auth, instructorAndAdmin, CourseController.show);
+
+router.get("/:courseId/edit", auth, instructorAndAdmin, CourseController.edit);
+
+router.put("/:courseId", auth, instructorAndAdmin, CourseController.update);
+
+router.post("/:courseId/change-status", auth, instructorAndAdmin, CourseController.changeStatus);
+
+router.delete("/:courseId", auth, instructorAndAdmin, CourseController.remove);
+
+router.post("/", auth, instructorAndAdmin, CourseController.create);
+
+module.exports = router;
