876344138d3e22a310b5e68005b11b0a0f3979a7
tersawy <mohamedtersawy96@gmail.com>
2021-09-03 15:36:03 +0200

    Added Setting Controller

---
+++ server/Controllers/SettingController.js
@@ -0,0 +1,9 @@
+const Setting = require("../Models/Setting");
+
+exports.index = (req, res) => {};
+
+exports.edit = (req, res) => {};
+
+exports.show = (req, res) => {};
+
+exports.update = (req, res) => {};
