33bfacdb036c78bb06c7405985959e10f336a3ed
tersawy <mohamedtersawy96@gmail.com>
2021-09-03 19:47:20 +0200

    Added Setting Seeder

---
+++ server/database/seeds/SettingSeed.js
@@ -0,0 +1,16 @@
+const Setting = require("../../Models/Setting");
+
+module.exports = {
+	model: Setting,
+	data: [
+		{
+			appName: "Mastery-English-Learning",
+			logoDark: "logo-dark.png",
+			logoLight: "logo-light.png",
+			homePage: {
+				categories: ["6126a51da0f256607f3a6aa0", "6126a51da0f256607f3a6aa1"],
+				headerBg: "header.jpeg",
+			},
+		},
+	],
+};
