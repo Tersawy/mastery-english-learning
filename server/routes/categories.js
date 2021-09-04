ee30a1b4385074252153984f990faee565c721dd
tersawy <mohamedtersawy96@gmail.com>
2021-09-05 00:48:02 +0200

    Added Categories routes

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -8,4 +8,8 @@ router.get("/options", CategoryController.options);
 
 router.post("/", CategoryController.create);
 
+router.put("/:categoryId", CategoryController.update);
+
+router.delete("/:categoryId", CategoryController.remove);
+
 module.exports = router;
