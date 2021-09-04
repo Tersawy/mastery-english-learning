9c08a9fcc978892a1ae9254159dc891659eea817
tersawy <mohamedtersawy96@gmail.com>
2021-09-04 15:02:59 +0200

    Added home page data

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,37 +2,13 @@
 	<div>
 		<Header />
 		<b-container fluid class="px-xl-5 py-5">
-			<div class="listening">
-				<h3>Listening :-</h3>
+			<div v-for="(category, i) in homePage.categories" :key="i" class="listening pb-5">
+				<h3>{{ category.name }} :-</h3>
 				<b-row>
 					<b-col cols="10" offset="1" class="mt-3">
 						<b-row cols="3" class="justify-content-center">
-							<b-col>
-								<CourseCard />
-							</b-col>
-							<b-col>
-								<CourseCard />
-							</b-col>
-							<b-col>
-								<CourseCard />
-							</b-col>
-						</b-row>
-					</b-col>
-				</b-row>
-			</div>
-			<div class="speaking pt-5">
-				<h3>Speaking :-</h3>
-				<b-row>
-					<b-col cols="10" offset="1" class="mt-3">
-						<b-row cols="3" class="justify-content-center">
-							<b-col>
-								<CourseCard />
-							</b-col>
-							<b-col>
-								<CourseCard />
-							</b-col>
-							<b-col>
-								<CourseCard />
+							<b-col v-for="(course, i) in category.courses" :key="i">
+								<CourseCard :course="course" />
 							</b-col>
 						</b-row>
 					</b-col>
@@ -47,7 +23,16 @@
 	import CourseCard from "@/components/home/CourseCard";
 	export default {
 		name: "Home",
-		components: { Header, CourseCard }
+		components: { Header, CourseCard },
+
+		mounted() {
+			this.$store.dispatch("getHomePage");
+		},
+		computed: {
+			homePage() {
+				return this.$store.state.pages.home;
+			}
+		}
 	};
 </script>
 
