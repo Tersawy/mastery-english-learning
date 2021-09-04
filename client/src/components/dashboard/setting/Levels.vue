e50ad84eeed237be5b34b371e8283227072d5092
tersawy <mohamedtersawy96@gmail.com>
2021-09-05 01:04:41 +0200

    Added Levels component

---
+++ client/src/components/dashboard/setting/Levels.vue
@@ -0,0 +1,71 @@
+<template>
+	<b-card no-body class="h-100">
+		<template #header>
+			<div class="d-flex justify-content-between align-items-center">
+				<span>Levels</span>
+				<b-btn variant="outline-primary" size="sm" @click="create">Create</b-btn>
+			</div>
+		</template>
+		<b-table hover responsive :items="items" :fields="fields" class="mb-0">
+			<template #cell(actions)="row">
+				<b-icon icon="pencil-square" scale="1.2" variant="success" class="c-pointer" @click="update(row.item)" />
+				<b-icon icon="trash" scale="1.2" variant="danger" class="ml-3 c-pointer" @click="remove(row.item)" />
+			</template>
+		</b-table>
+		<LevelForm />
+		<DeleteFieldModal msg="Are you sure to delete this level ?" @done="removeLevel" modal-id="deleteLevelModal" />
+	</b-card>
+</template>
+
+<script>
+	import LevelForm from "@/components/dashboard/setting/LevelForm";
+	import dataTableMixin from "@/mixins/dataTableMixin";
+	import DeleteFieldModal from "@/components/DeleteFieldModal.vue";
+	export default {
+		components: { LevelForm, DeleteFieldModal },
+
+		mixins: [dataTableMixin],
+
+		data() {
+			return {
+				namespace: "Level",
+				fields: [
+					{ key: "name", label: "Name" },
+					{ key: "actions", label: "Actions" }
+				]
+			};
+		},
+		methods: {
+			create() {
+				this.$store.commit("Level/setOne", {});
+				this.$bvModal.show("levelForm");
+			},
+
+			update(level) {
+				this.$store.commit("Level/setOne", level);
+				this.$bvModal.show("levelForm");
+			},
+
+			remove(level) {
+				this.$store.commit("Level/setOne", level);
+				this.$bvModal.show("deleteLevelModal");
+			},
+
+			async removeLevel() {
+				try {
+					let res = await this.$store.dispatch("Level/remove", this.$store.state.Level.one);
+
+					if (res && res.msg) this.setGlobalSuccess(res.msg);
+
+					this.$nextTick(() => {
+						this.$bvModal.hide("deleteLevelModal");
+					});
+				} catch (err) {
+					//
+				}
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped></style>
