4a539f4fb6be0a72b1fb03d736ea5f80e75e4f4a
tersawy <mohamedtersawy96@gmail.com>
2021-09-05 18:21:46 +0200

    Added student enroll action

--- client/src/store/modules/Student/actions.js
+++ client/src/store/modules/Student/actions.js
@@ -1,1 +1,13 @@
-export default {};
+import api from "@/plugins/api";
+
+export default {
+	enroll({ state, commit }, course) {
+		return api("post", `${state.prefix}/enroll`, { courseId: course._id }, (err, data) => {
+			if (err) {
+				commit("setErrors", err);
+				return Promise.reject(err);
+			}
+			return Promise.resolve(data);
+		});
+	}
+};
