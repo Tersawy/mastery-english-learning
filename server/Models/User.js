fcbbcac74d2a59b0ccde145346995963b99eb317
tersawy <mohamedtersawy96@gmail.com>
2021-09-06 14:15:07 +0200

    Added image field to User Model

--- server/Models/User.js
+++ server/Models/User.js
@@ -36,6 +36,7 @@ const userSchema = new Schema(
 		},
 		password: { type: String, required: [true, "Password is required"] },
 		type: { type: Number, default: USER_STUDENT },
+		image: { type: String, default: "empty.jpg" },
 		courses: [{ type: "ObjectId", ref: "Course" }], // for students only
 		remmemberToken: { type: String, default: null },
 		expiresToken: { type: Date, default: Date.now },
