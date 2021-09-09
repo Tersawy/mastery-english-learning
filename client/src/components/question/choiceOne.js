4613eebbe8406d2df3e8ac62882a2a4bb85568d9
tersawy <mohamedtersawy96@gmail.com>
2021-09-09 13:26:42 +0200

    Fix question choice one component

--- client/src/components/question/choiceOne.js
+++ client/src/components/question/choiceOne.js
@@ -5,11 +5,8 @@ export default {
 			<div class="question question-Choice d-flex flex-column align-items-start" style="line-height: 1.5">
 				<span>{this.question.text}</span>
 				<b-form-radio-group
-					options={this.question.Choices}
+					options={this.question.choices}
 					class="Choice-radio w-100 d-flex justify-content-around mt-3"
-					value-field="_id"
-					text-field="text"
-					disabled-field="notEnabled"
 					onChange={(value) => this.$emit("input", value)}
 				></b-form-radio-group>
 			</div>
