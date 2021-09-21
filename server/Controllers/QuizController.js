1f3ca73df48e03543b378ff386efbd0b13034491
tersawy <mohamedtersawy96@gmail.com>
2021-09-21 19:41:18 +0200

    Added quiz answers to show methods if user is student

--- server/Controllers/QuizController.js
+++ server/Controllers/QuizController.js
@@ -1,3 +1,4 @@
+const mongoose = require( "mongoose" );
 const Quiz = require("../Models/Quiz");
 
 exports.create = async (req, res) => {
@@ -15,9 +16,65 @@ exports.create = async (req, res) => {
 exports.show = async (req, res) => {
 	let { lectureId: lecture } = req.params;
 
-	let quiz = await Quiz.findOne({ lecture });
+	let { me, isStudent } = req.body;
 
-	res.json(quiz || {});
+	if (!isStudent) {
+		let quiz = await Quiz.findOne({ lecture });
+
+		return res.json(quiz || {});
+	}
+
+	let aggregate = [
+		{ $match: { lecture: mongoose.Types.ObjectId(lecture) } },
+		{
+			$lookup: {
+				from: "quizanswers",
+				let: { quizId: "$_id" },
+				as: "quizAnswer",
+				pipeline: [
+					{ $match: { $expr: { $eq: ["$quiz", "$$quizId"], $eq: ["$student", me._id] } } }
+				]
+			}
+		},
+		{ $unwind: { path: "$quizAnswer", preserveNullAndEmptyArrays: true} }
+	];
+
+	let quiz = await Quiz.aggregate(aggregate)
+	
+	quiz = quiz[0]
+	
+	if (!quiz) return res.json({});
+	
+	quiz.questions = quiz.questions.map(question => {
+		let q = {};
+
+		q._id = question._id;
+		q.choices = question.choices;
+		q.text = question.text;
+		q.type = question.type;
+
+		if (!quiz.quizAnswer) return q;
+
+		let answer = quiz.quizAnswer.answers.find(a => String(a.question) == String(question._id));
+
+		if (answer) {
+			q.isCorrected = answer.isCorrected;
+			q.isTrue = answer.isTrue;
+			q.answer = answer.value;
+			q.defaultAnswer = question.answer;
+		}
+
+		return q;
+	})
+	
+	quiz = {
+		_id: quiz._id,
+		isAnswered: !!quiz.quizAnswer,
+		lecture: quiz.lecture,
+		questions: quiz.questions
+	}
+	
+	res.json(quiz);
 };
 
 exports.remove = async (req, res) => {
