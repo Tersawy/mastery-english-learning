3c0107483af85e78f5ae5e51615e08e73aa1222d
tersawy <mohamedtersawy96@gmail.com>
2021-09-30 20:40:31 +0200

    Separate login form into sparated component

--- client/src/views/Auth/Login.vue
+++ client/src/views/Auth/Login.vue
@@ -11,17 +11,7 @@
 		<b-row class="mt-4 w-100 justify-content-center">
 			<b-col cols="12" sm="8" lg="6" xl="4">
 				<b-card class="shadow">
-					<b-form @submit.prevent="handleLogin">
-						<b-form-group label="Email address" label-for="email">
-							<b-form-input type="text" id="email" v-model.trim="login.email" />
-						</b-form-group>
-						<b-form-group label="Password" label-for="password">
-							<b-form-input type="password" id="password" v-model="login.password" />
-						</b-form-group>
-						<b-overlay :show="isLoading" rounded opacity="0.6" spinner-small spinner-variant="primary" @hidden="toggleLoading">
-							<b-btn type="submit" :disabled="isLoading" block variant="primary" class="mt-3" @click="handleLogin">Sign in</b-btn>
-						</b-overlay>
-					</b-form>
+					<LoginForm @signedIn="() => $router.push('/')" />
 				</b-card>
 			</b-col>
 		</b-row>
@@ -29,51 +19,12 @@
 </template>
 
 <script>
-	import { required, email, minLength, maxLength } from "vuelidate/lib/validators";
+	import LoginForm from "@/components/auth/LoginForm";
 	export default {
-		data() {
-			return {
-				login: {
-					email: null,
-					password: null,
-					remember: false
-				}
-			};
-		},
-
-		validations() {
-			let login = {
-				email: { required, email },
-				password: { required, minLength: minLength(8), maxLength: maxLength(54) }
-			};
-
-			return { login };
-		},
+		components: { LoginForm },
 
 		mounted() {
 			this.$store.commit("setLoader", false);
-		},
-
-		methods: {
-			async handleLogin() {
-				this.$v.$touch();
-
-				if (!this.login.email || !this.login.password) return;
-
-				if (this.$v.login.$invalid) {
-					return this.setGlobalError("Email or password are not valid");
-				}
-
-				this.toggleLoading();
-
-				try {
-					await this.$store.dispatch("Auth/login", this.login);
-				} catch (err) {
-					//
-				}
-
-				this.toggleLoading();
-			}
 		}
 	};
 </script>
