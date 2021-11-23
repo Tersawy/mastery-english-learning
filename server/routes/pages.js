dee7dc480377c51dc124811b7d7a03495f063260
Tersawy <mohamedtersawy96@gmail.com>
2021-11-23 14:20:07 -0500

    Added dashboard to Pages routes

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 
 const PagesController = require("../Controllers/PagesController");
 
-const { getAuth } = require("../middlewares/auth");
+const { ADMIN, INSTRUCTOR, OWNER } = require("../helpers/constants");
+
+const { getAuth, auth, allowedFor } = require("../middlewares/auth");
 
 router.get("/home", getAuth, PagesController.home);
+
+router.get("/dashboard", auth, allowedFor(ADMIN, INSTRUCTOR, OWNER), PagesController.dashboard);
 
 module.exports = router;
