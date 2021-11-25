432638faa57d5fd951024bf38822e56cc5440578
Tersawy <mohamedtersawy96@gmail.com>
2021-11-25 09:27:17 -0500

    Added course detail view

---
+++ client/src/views/Dashboard/Course/detail.vue
@@ -0,0 +1,78 @@
+<template>
+	<dashboard-layout class="courses">
+		<b-breadcrumb class="bg-white border rounded shadow-sm font-weight-600 mb-30px">
+			<b-breadcrumb-item to="/dashboard">
+				<b-icon class="mr-1" icon="house-fill" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+				Dashboard
+			</b-breadcrumb-item>
+			<b-breadcrumb-item to="/dashboard/courses"> Courses </b-breadcrumb-item>
+			<b-breadcrumb-item active> {{ course.title }} </b-breadcrumb-item>
+		</b-breadcrumb>
+
+		<b-row cols="1" cols-md="3">
+			<b-col class="mb-4 mb-lg-0">
+				<report-card name="Number of Sections" icon="collection" :number="course.sectionsCount" />
+			</b-col>
+			<b-col class="mb-4 mb-lg-0">
+				<report-card name="Number of Lectures" icon="bricks" :number="course.lecturesCount" />
+			</b-col>
+			<b-col class="mb-4 mb-lg-0">
+				<report-card name="Number of Students" icon="people" :number="course.studentsCount" />
+			</b-col>
+		</b-row>
+
+		<b-row class="mt-lg-30px">
+			<b-col lg="8" md="12" order="2" order-lg="1" class="mt-30px mt-lg-0">
+				<CourseSections />
+			</b-col>
+			<b-col lg="4" md="12" order="1" order-lg="2">
+				<CourseStudents />
+			</b-col>
+			<b-col cols="12" class="mt-30px" order="3">
+				<CourseLectures />
+			</b-col>
+		</b-row>
+	</dashboard-layout>
+</template>
+
+<script>
+	import ReportCard from "@/components/dashboard/ReportCard";
+	import DashboardLayout from "@/components/layouts/DashboardLayout.vue";
+	import { COURSE_APPROVED } from "@/helpers/constants";
+	import { asyncHandler } from "@/helpers/functions";
+	import CourseStudents from "@/components/dashboard/course/CourseStudents.vue";
+	import CourseSections from "@/components/dashboard/course/CourseSections.vue";
+	import CourseLectures from "@/components/dashboard/course/CourseLectures.vue";
+	export default {
+		components: { DashboardLayout, ReportCard, CourseStudents, CourseSections, CourseLectures },
+
+		data() {
+			return {
+				namespace: "Course"
+			};
+		},
+
+		async mounted() {
+			if (!this.$route.params.courseId) {
+				return this.$router.push("/dashboard/courses");
+			}
+
+			asyncHandler(this.getCourseDetail, (err) => {
+				if (err) return this.$router.push("/dashboard/courses");
+				this.$store.commit("setLoader", false);
+			});
+		},
+
+		computed: {
+			course() {
+				return this.$store.state.Course.detail;
+			}
+		},
+
+		methods: {
+			getCourseDetail() {
+				return this.$store.dispatch("Course/getCourseDetail", this.$route.params.courseId);
+			}
+		}
+	};
+</script>
