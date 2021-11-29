5340420a9da547813f8643a790c6c857764d7351
Tersawy <mohamedtersawy96@gmail.com>
2021-11-29 16:05:43 +0200

    Fix call function from it self in helper functions

--- server/helpers/functions.js
+++ server/helpers/functions.js
@@ -66,14 +66,14 @@ exports.areTheyEqual = (a, b) => {
 		if (a.length !== b.length) return false;
 
 		for (let i = 0; i < a.length; i++) {
-			if (!areTheyEqual(a[i], b[i])) return false;
+			if (!this.areTheyEqual(a[i], b[i])) return false;
 		}
 		return true;
 	}
 
 	if (typeof a === "object") {
 		for (let key in a) {
-			if (!areTheyEqual(a[key], b[key])) return false;
+			if (!this.areTheyEqual(a[key], b[key])) return false;
 		}
 		return true;
 	}
