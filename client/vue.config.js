53b428c9c21cabbdc38915e6b53244a518eabe71
Tersawy <mohamedtersawy96@gmail.com>
2022-04-22 02:05:25 +0200

    Rebuild

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,5 +2,5 @@ const path = require("path");
 
 module.exports = {
 	lintOnSave: false,
-	outputDir: path.resolve(path.join(__dirname, "../", "server", "public", "dist"))
+	outputDir: path.resolve(path.join(__dirname, "../", "server", "public", "build"))
 };
